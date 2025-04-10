577dc226fba9032d3bf609e15a321590588fafcf
sahil-twinreality <sahil.singh@twinreality.in>
2025-04-10 16:57:18 +0530

    added keystore

---
+++ Assets/Scripts/OpenWebsite.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class OpenWebsite : MonoBehaviour
+{
+    // Call this function to open the URL
+    public void OpenTwinRealitySite()
+    {
+        Application.OpenURL("https://twinreality.in/");
+    }
+}
